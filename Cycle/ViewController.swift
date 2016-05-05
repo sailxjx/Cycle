1c42eb8bc2a5cf6b855c93221505d4c71de79102
Xu Jingxin <sailxjx@gmail.com>
2016-05-05 15:13:09 +0800

    Initial Commit

---
+++ Cycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cycle
+//
+//  Created by tristan on 16/5/5.
+//  Copyright © 2016年 Xu Jingxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
